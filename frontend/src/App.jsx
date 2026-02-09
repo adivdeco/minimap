a9e735d4d51173145558c66c90b0c796c8d8a0a6
adivdeco <sadiv120@gmail.com>
2026-02-09 22:51:53 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
               pauseOnFocusLoss
               draggable
               pauseOnHover
-              theme="dark"
+              theme="colored"
             />
           </AuthProvider>
         </ThemeProvider>
